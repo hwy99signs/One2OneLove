aefd15b0372d6132891ba67c8725ad298f9ced0d
hwy99signs <hwy99signs@gmail.com>
2026-09-07 17:22:20 -0500

    ui: apply One2One Love name globally

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
 import '@/index.css'
+import '@/brandName.css'
+import { installBrandNameNormalizer } from '@/brandName.js'
+
+installBrandNameNormalizer()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
