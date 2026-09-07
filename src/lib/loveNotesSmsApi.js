9d980be961937334c0ae42b0b1a93ef928622c11
hwy99signs <hwy99signs@gmail.com>
2026-09-07 18:26:57 -0500

    revert: remove unnecessary Love Notes SMS client

--- src/lib/loveNotesSmsApi.js
+++
@@ -1,9 +0,0 @@
-import { apiRequest } from './one2oneApi';
-
-export async function sendLoveNoteSms(payload) {
-  const data = await apiRequest('/api/love-notes/send-sms', {
-    method: 'POST',
-    body: payload,
-  });
-  return data?.message || null;
-}
