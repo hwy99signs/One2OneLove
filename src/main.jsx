925bac6ccfe7ef95db6e5c9f10141babdbb5031b
hwy99signs <hwy99signs@gmail.com>
2026-09-07 17:26:55 -0500

    fix: restore app startup after brand normalization change

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
 import '@/index.css'
-import '@/brandName.css'
-import { installBrandNameNormalizer } from '@/brandName.js'
-
-installBrandNameNormalizer()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
-) 
+)
