079652a7f921b5cfd270572ee491b1d930db20c8
hwy99signs <hwy99signs@gmail.com>
2026-09-07 13:34:41 -0500

    ui: point Facebook footer icon to One2OneLove page

--- src/components/home/Footer.jsx
+++ src/components/home/Footer.jsx
@@ -108,7 +108,7 @@ export default function Footer() {
             </p>
             <div className="flex gap-3">
               <a
-                href="https://facebook.com"
+                href="https://www.facebook.com/profile.php?id=61583726951948"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="w-10 h-10 bg-blue-600 rounded-full flex items-center justify-center hover:bg-blue-700 transition-colors"
