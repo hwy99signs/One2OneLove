7b4e53284717397af5e226732e025426233389a6
hwy99signs <hwy99signs@gmail.com>
2026-09-11 21:22:19 -0500

    Use visible launch account form

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { createPageUrl } from "@/utils";
-import LaunchRegularUserForm from "@/components/signup/LaunchRegularUserForm";
+import LaunchAccountForm from "@/components/signup/LaunchAccountForm";
 
 export default function SignUp() {
   const navigate = useNavigate();
@@ -12,7 +12,7 @@ export default function SignUp() {
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-pink-100 via-purple-100 to-blue-100 py-12 px-4">
-      <LaunchRegularUserForm onBack={handleBack} />
+      <LaunchAccountForm onBack={handleBack} />
     </div>
   );
 }
