fe17fb82f172d8d15cee86f0f23edf104c2baeb2
hwy99signs <hwy99signs@gmail.com>
2026-09-12 20:22:57 -0500

    Remove unused organization signup route wrapper

--- src/pages/OrganizationSignup.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ProfessionalApplication from "@/components/signup/ProfessionalApplication";
-
-export default function OrganizationSignup() {
-  return <ProfessionalApplication mode="organization" />;
-}
