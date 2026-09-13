28b0d4fa831c107a47d832c31c79223dd662016c
hwy99signs <hwy99signs@gmail.com>
2026-09-12 20:22:51 -0500

    Remove unused contributor signup route wrapper

--- src/pages/ContributorSignup.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ProfessionalApplication from "@/components/signup/ProfessionalApplication";
-
-export default function ContributorSignup() {
-  return <ProfessionalApplication mode="contributor" />;
-}
