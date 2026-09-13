aca38921e53e67ea010b94a5e916c6f8a976b92e
hwy99signs <hwy99signs@gmail.com>
2026-09-12 20:22:45 -0500

    Remove unused coach signup route wrapper

--- src/pages/RelationshipProfessionalSignup.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ProfessionalApplication from "@/components/signup/ProfessionalApplication";
-
-export default function RelationshipProfessionalSignup() {
-  return <ProfessionalApplication mode="coach" />;
-}
