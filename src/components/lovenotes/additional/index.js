81215169cdb61ef45ae009712a759808508cf63b
hwy99signs <hwy99signs@gmail.com>
2026-09-13 10:37:27 -0500

    Add multilingual Love Notes additions index

---
+++ src/components/lovenotes/additional/index.js
@@ -0,0 +1,7 @@
+import en from "./LoveNotesAdditional.en";
+import es from "./LoveNotesAdditional.es";
+import fr from "./LoveNotesAdditional.fr";
+import it from "./LoveNotesAdditional.it";
+import de from "./LoveNotesAdditional.de";
+
+export const additionalLoveNotesData = { en, es, fr, it, de };
