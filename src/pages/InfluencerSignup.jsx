72c84bd1430699d701e4f536b9a7aec14c502fb8
hwy99signs <hwy99signs@gmail.com>
2026-09-12 21:35:22 -0500

    Close contributor language menu naturally

--- src/pages/InfluencerSignup.jsx
+++ src/pages/InfluencerSignup.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ContributorProfessionalApplication from "@/components/signup/ContributorProfessionalApplication";
+import DismissibleDetailsBoundary from "@/components/signup/DismissibleDetailsBoundary";
 
 export default function InfluencerSignup() {
-  return <ContributorProfessionalApplication />;
+  return <DismissibleDetailsBoundary><ContributorProfessionalApplication /></DismissibleDetailsBoundary>;
 }
