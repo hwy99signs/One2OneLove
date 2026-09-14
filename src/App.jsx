f7fc7430967e492123017c13e629b2154536260a
hwy99signs <hwy99signs@gmail.com>
2026-09-14 18:39:49 -0500

    Mount subscription redirect notice

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Admin from "@/pages/Admin.jsx"
 import Analytics from "@/pages/Analytics.jsx"
 import LoveNotesLimitAddSends from "@/components/lovenotes/LoveNotesLimitAddSends.jsx"
 import DateIdeasPlanAccess from "@/components/dateideas/DateIdeasPlanAccess.jsx"
+import SubscriptionLaunchNotice from "@/components/subscriptions/SubscriptionLaunchNotice.jsx"
 import ProfileLaunchFixes from "@/components/profile/ProfileLaunchFixes.jsx"
 import LaunchSurfaceCleanup from "@/components/launch/LaunchSurfaceCleanup.jsx"
 import AdminFeatureHeaderLock from "@/components/admin/AdminFeatureHeaderLock.jsx"
@@ -52,6 +53,7 @@ function App() {
         <AdminUserModeToggle />
         <LoveNotesLimitAddSends />
         <DateIdeasPlanAccess />
+        <SubscriptionLaunchNotice />
         <ProfileLaunchFixes />
         <LaunchSurfaceCleanup />
         <AdminFeatureHeaderLock />
