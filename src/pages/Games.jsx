5de40c2291d088cd91afa7887d778e18fedd2e51
hwy99signs <hwy99signs@gmail.com>
2026-09-15 20:01:55 -0500

    Undo WSTD Up Next preview layout

--- src/pages/Games.jsx
+++ src/pages/Games.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { createPageUrl } from "@/utils";
-import WhatShouldTheyDoGameUpNext from "../features/whatShouldTheyDo/WhatShouldTheyDoGameUpNext";
+import WhatShouldTheyDoGameHybridReadable from "../features/whatShouldTheyDo/WhatShouldTheyDoGameHybridReadable";
 
 export default function Games() {
   const navigate = useNavigate();
 
   return (
-    <WhatShouldTheyDoGameUpNext
+    <WhatShouldTheyDoGameHybridReadable
       onExit={() => navigate(createPageUrl("Home"))}
     />
   );
