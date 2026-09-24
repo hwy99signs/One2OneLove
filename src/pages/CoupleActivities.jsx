a9e9cc77f0d4ada34a3b11b4adfae3f8455c9819
hwy99signs <hwy99signs@gmail.com>
2026-09-23 19:06:41 -0500

    Add Games entry to Couple Activities

--- src/pages/CoupleActivities.jsx
+++ src/pages/CoupleActivities.jsx
@@ -219,6 +219,15 @@ export default function CoupleActivities() {
       category: 'learning'
     },
     {
+      id: 'game',
+      name: t.activities_types.game,
+      description: t.activities_types.game_desc,
+      icon: Gamepad2,
+      color: 'from-green-500 to-emerald-600',
+      link: 'CooperativeGames',
+      category: 'play'
+    },
+    {
       id: 'goal',
       name: t.activities_types.goal,
       description: t.activities_types.goal_desc,
