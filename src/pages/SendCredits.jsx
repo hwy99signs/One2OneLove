9b9553bf80fb597432a4b38e944e6366206c103f
hwy99signs <hwy99signs@gmail.com>
2026-09-25 00:43:50 -0500

    Retire extra-send credit purchase page

--- src/pages/SendCredits.jsx
+++ src/pages/SendCredits.jsx
@@ -3,10 +3,8 @@ import { useNavigate } from 'react-router-dom';
 
 export default function SendCredits() {
   const navigate = useNavigate();
-
   useEffect(() => {
     navigate('/Subscription', { replace: true });
   }, [navigate]);
-
   return null;
 }
