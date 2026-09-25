0e7cb044947958c5faf25d94dc24ebfe1555f16c
hwy99signs <hwy99signs@gmail.com>
2026-09-25 15:15:55 -0500

    Extend admin MFA idle window to 30 minutes

--- worker/admin-mfa.ts
+++ worker/admin-mfa.ts
@@ -8,7 +8,7 @@ const JSON_HEADERS = {
 };
 
 const COOKIE_NAME = '__Host-o2ol_admin_mfa';
-const MFA_TTL_SECONDS = 5 * 60;
+const MFA_TTL_SECONDS = 30 * 60;
 
 function json(data, status = 200, extraHeaders = {}) {
   return new Response(JSON.stringify(data), {
